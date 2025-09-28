61e54b2b934a7e2967a52127fa943e8a5c9cc35a
MUKESH <119718289+Mukesh12221855@users.noreply.github.com>
2025-09-28 11:56:33 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/React-Bike-Rental">
       <App />
     </Router>
   </React.StrictMode>
